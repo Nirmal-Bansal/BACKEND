3046500ece4d9e68ece7bb3be3e5632956feb2d9
Nirmal <nirmal.bansal_cs24@gla.ac.in>
2026-02-17 15:10:53 +0530

    notes application

--- Assignments/Assignment3/Practice-Project1/Notes-Server/server.js
+++ Assignments/Assignment3/Practice-Project1/Notes-Server/server.js
@@ -1,11 +1,48 @@
-const http = require("http");
-const server = http.createServer((req,res) => {
-    res.writeHead(200, {"Content-Type":"text/plain"});
-    res.end("server is listening");
-})
+const http =require("http");
+const fs =require("fs");
+const path = require("path");
+const { json } = require("stream/consumers");
+const filePath = path.join(__dirname, "notes.json");
 
-const url = new URL(req.url,"http://${req.headers.host}");
-const pathname = url.pathname;
+const server = http.createServer((req, res) => {
+    const url =new URL(req.url,`http://${req.headers.host}`);
+    const pathname = url.pathname;
+    const method = req.method;
 
-server.listen(3000,() =>{console.log("Server is running on http://localhost:3000")
+
+    if(method === "GET" && pathname === "/") {
+      res.writeHead(200, "server responded")
+      res.end()
+    } else if(method == "GET" && pathname == "/notes") {
+      fs.readFile(filePath, "utf-8", (err, data) => {
+        if(err) {
+          res.writeHead(500, "cannot get data")
+          res.end()
+        } else {
+          res.writeHead(200, {"content-type" : "application/json"});
+          res.end(data);
+        }
+      })
+    } else if(method == "POST" && pathname == "/notes") {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk;
+      })
+
+      req.on("end", () => {
+        fs.readFile(filePath, "utf-8", (err, data) => {
+          const notes = JSON.parse(data);
+          const parsedBody = JSON.parse(body);
+          notes.push(parsedBody)
+          fs.writeFile(filePath, JSON.stringify(notes), ()=> {
+            res.writeHead(200, "done!")
+            res.end()
+          })
+        })
+      })
+    }
+
+});
+
+server.listen(3000,() => {console.log("Server running on http://localhost:3000");
 });
